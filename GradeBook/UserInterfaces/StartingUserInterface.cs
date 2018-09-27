740d6303a2254cb8e9afbdf0557fb1b454b2ea29
Jason Aurich <jaurich@ALLEGIANCE.LOCAL>
2018-09-27 10:01:49 -0600

    Completed work

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,7 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            var weighted = bool.Parse(parts[3])
+            var weighted = bool.Parse(parts[3]);
             BaseGradeBook gradeBook;
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name, weighted);
